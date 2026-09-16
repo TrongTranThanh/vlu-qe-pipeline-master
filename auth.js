b1100c2fd6a7486d86faf17134d242a77270cc3b
Trần Thanh Trọng <trong.2474802010408@vanlanguni.vn>
2026-09-16 16:45:21 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ function login(username, password) {
     }
 
     // Kiểm tra đăng nhập thành công
-    if (username === 'admin' && password === '999') {
+    if (username === 'admin' && password === '123') {
         return true;
     }
 
